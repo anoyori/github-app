036c07ba2daabc8f3e50aa176ffdc954734c0369
anoyori <anoyori@gmail.com>
2021-12-29 01:44:11 -0300

    Adicionando marcação com html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,46 @@
 'use strict'
 
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  render () {
-    return (
-      <Counter />
-    )
-  }
-}
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input
+        type='search'
+        placeholder='Digite o nome do usuário'
+      />
+    </div>
+    <div className='user-info'>
+      <img src='https://avatars.githubusercontent.com/u/80289735?v=4' />
+      <h1>
+        <a href='https://github.com/anoyori'>Amanda Noyori</a>
+      </h1>
+
+      <ul className='repos-info'>
+        <li>Repositórios: 30</li>
+        <li>Seguidores: 34</li>
+        <li>Seguindo: 46</li>
+      </ul>
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Favoritos:</h2>
+        <ul className='starred'>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
 
 export default App
